541faca018f6a4560bd31a453ac1e1c796414d6d
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-07 19:27:45 -0300

    back

---
+++ back/index.js
@@ -0,0 +1,20 @@
+import app from "./src/app.js";
+import { connectDB } from "./src/db.js";
+
+const port = process.env.PORT || 3000;
+
+connectDB();
+
+app.listen(port, () => {
+  console.log(`server on pory ${port}`);
+});
+
+// import app from "./app.js";
+// import { connectDB } from "./db.js";
+
+// connectDB();
+// app.listen(3000)
+// console.log("server on pory", 3000)
+
+
+// "dev": "nodemon src/index.js"
