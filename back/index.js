db8532acd873696d7e9a44ef92f4ecd0ee012701
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-07 19:15:52 -0300

    index.js

--- back/index.js
+++
@@ -1,7 +0,0 @@
-import app from "./src/app.js";
-import { connectDB } from "./db.js";
-
-connectDB();
-app.listen(3000)
-console.log("server on pory", 3000)
-
--- back/src/index.js
+++ back/src/index.js
@@ -1,7 +1,20 @@
+// import app from "./app.js";
+// import { connectDB } from "./db.js";
+
+// connectDB();
+// app.listen(3000)
+// console.log("server on pory", 3000)
+
+////////////////////////////////////////////
+
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
+const port = process.env.PORT || 3000;
+
 connectDB();
-app.listen(3000)
-console.log("server on pory", 3000)
+
+app.listen(port, () => {
+  console.log(`server on pory ${port}`);
+});
 
