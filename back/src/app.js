c25ba201d71b45ffd35426b1a7e983476b3ec747
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-07 21:13:56 -0300

    fdsf

--- back/src/app.js
+++ back/src/app.js
@@ -1,24 +1,56 @@
+// import express from "express";
+// import morgan from "morgan";
+// import cookieParser from "cookie-parser"
+// import authRoutes from "./routes/authRoute.js";
+// import tasksRoutes from "./routes/tasksRoutes.js";
+// import completeProfile from "./routes/completeProfile.js";
+// import cors from "cors"
+
+// const app = express()
+
+// app.use(morgan("dev"))
+// app.use(express.json())
+// app.use(cookieParser());
+
+// app.use(cors({
+//     origin: ["http://localhost:5173", "https://red-social-tc3n.vercel.app"], // en este caso le decimos que solo ese es puede comunicar
+//     credentials: true // para permitir ver tmb las cookies
+// })); //quiero permitir que todos los dominios se puedan comunicar en este servidor.
+// app.use("/api", authRoutes)
+// app.use("/api", tasksRoutes)
+// app.use("/api", completeProfile)
+
+// export default app; 
+
 import express from "express";
 import morgan from "morgan";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/authRoute.js";
 import tasksRoutes from "./routes/tasksRoutes.js";
 import completeProfile from "./routes/completeProfile.js";
-import cors from "cors"
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(morgan("dev"))
-app.use(express.json())
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(cookieParser());
 
+// Configuración de CORS
 app.use(cors({
-    origin: ["http://localhost:5173", "https://red-social-tc3n.vercel.app"], // en este caso le decimos que solo ese es puede comunicar
-    credentials: true // para permitir ver tmb las cookies
-})); //quiero permitir que todos los dominios se puedan comunicar en este servidor.
-app.use("/api", authRoutes)
-app.use("/api", tasksRoutes)
-app.use("/api", completeProfile)
+    origin: ["http://localhost:5173", "https://red-social-8ysd.vercel.app"],
+    credentials: true
+}));
 
-export default app; 
+// Manejador de OPTIONS para todas las rutas
+app.options('*', cors());
+
+// Rutas
+app.use("/api", authRoutes);
+app.use("/api", tasksRoutes);
+app.use("/api", completeProfile);
+
+// Manejo de otras rutas o configuraciones...
+
+export default app;
 
