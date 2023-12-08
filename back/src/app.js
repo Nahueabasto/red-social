42a7fae52bc17686ea129d871f70ddcdce3ab934
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-07 21:23:14 -0300

    dsd

--- back/src/app.js
+++ back/src/app.js
@@ -22,35 +22,41 @@
 
 // export default app; 
 
-import express from "express";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import authRoutes from "./routes/authRoute.js";
-import tasksRoutes from "./routes/tasksRoutes.js";
-import completeProfile from "./routes/completeProfile.js";
-import cors from "cors";
+// app.js
+// app.js
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import authRoutes from './routes/authRoute.js';
+import tasksRoutes from './routes/tasksRoutes.js';
+import completeProfile from './routes/completeProfile.js';
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 
-// Configuración de CORS
-app.use(cors({
-    origin: ["http://localhost:5173", "https://red-social-8ysd.vercel.app"],
-    credentials: true
-}));
+// Definir opciones de CORS
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://red-social-8ysd.vercel.app"],
+  credentials: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+};
 
-// Manejador de OPTIONS para todas las rutas
-app.options('*', cors());
+// Crear middleware de CORS
+const corsMiddleware = cors(corsOptions);
+
+// Aplicar middleware de CORS
+app.use(corsMiddleware);
 
 // Rutas
-app.use("/api", authRoutes);
-app.use("/api", tasksRoutes);
-app.use("/api", completeProfile);
+app.use('/api', authRoutes);
+app.use('/api', tasksRoutes);
+app.use('/api', completeProfile);
 
 // Manejo de otras rutas o configuraciones...
 
 export default app;
-
