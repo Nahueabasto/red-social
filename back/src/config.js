6855ec39f8d7bafd6711cfb62db2ff286642897c
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-12 17:13:49 -0300

    fsd

--- back/src/config.js
+++ back/src/config.js
@@ -1,1 +1,8 @@
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+// export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
+
+import crypto from 'crypto';
+
+// Generar una cadena aleatoria de 32 bytes
+export const TOKEN_SECRET = crypto.randomBytes(32).toString('hex');
+
+console.log(TOKEN_SECRET);
