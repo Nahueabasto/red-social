d49503a3ad398ddf6f799ba2dc93e77c4e3c5085
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2023-12-14 19:57:49 -0300

    fsd

--- nahuok/src/api/auth.js
+++ nahuok/src/api/auth.js
@@ -2,20 +2,42 @@ import axios from "./axios";
 
 // const API = "http://localhost:3000/api"
 
-export const registerRequest = user => axios.post(`/register`, user);
+export const registerRequest = user => axios.post(`/register`, user, { withCredentials: true });
 
-// export const LoginRequest = user => axios.post(`/login`, user);
-export const loginRequest = user => {
-    const url = "/login";
-    const data = user;
-  
-    return axios.post(url, data, { withCredentials: true });
-  };
+export const LoginRequest = user => axios.post(`/login`, user, { withCredentials: true });
 
-export const verifyTokenRequest = () => axios.get("/verify")
+export const verifyTokenRequest = () => axios.get("/verify", { withCredentials: true })
 
-export const deleteProfileRequest = (id) => axios.delete(`/deleteProfile/${id}`)
+export const deleteProfileRequest = (id) => axios.delete(`/deleteProfile/${id}`, { withCredentials: true })
 
-//export const LogoutRequest = () => axios.get("/verify")
+export const LogoutRequest = () => axios.get("/verify", { withCredentials: true })
 
 //esperamos que el back nos confirme si el usuario esta autenticado o no
+
+// import axios from "./axios";
+
+// export const registerRequest = user => {
+//   const url = "/register";
+//   const data = user;
+
+//   return axios.post(url, data, { withCredentials: true });
+// };
+
+// export const loginRequest = user => {
+//   const url = "/login";
+//   const data = user;
+
+//   return axios.post(url, data, { withCredentials: true });
+// };
+
+// export const verifyTokenRequest = () => {
+//   const url = "/verify";
+
+//   return axios.get(url, { withCredentials: true });
+// };
+
+// export const deleteProfileRequest = id => {
+//   const url = `/deleteProfile/${id}`;
+
+//   return axios.delete(url, { withCredentials: true });
+// };
--- nahuok/src/api/profile.js
+++ nahuok/src/api/profile.js
@@ -1,12 +1,41 @@
 import axios from "./axios";
 
-export const getProfilesRequest = () => axios.get("/getProfiles") 
+export const getProfilesRequest = () => axios.get("/getProfiles", { withCredentials: true }) 
 
-export const createProfileRequest = (profile) => axios.post("/createProfile", profile)
+export const createProfileRequest = (profile) => axios.post("/createProfile", profile, { withCredentials: true })
 
-export const updatedProfileRequest = (id, profile) => axios.put(`/updatedProfile/${id}`, profile)
+export const updatedProfileRequest = (id, profile) => axios.put(`/updatedProfile/${id}`, profile, { withCredentials: true })
 
-export const getProfileRequest = (id) => axios.get(`/getProfile/${id}`) //va a servir para traerme los perfiles, NO SE SI VAMOS A TENER QUE MODIFICAR LA RUTA
+export const getProfileRequest = (id) => axios.get(`/getProfile/${id}`, { withCredentials: true }) //va a servir para traerme los perfiles, NO SE SI VAMOS A TENER QUE MODIFICAR LA RUTA
+
+// import axios from "./axios";
+
+// export const getProfilesRequest = () => {
+//   const url = "/getProfiles";
+
+//   return axios.get(url, { withCredentials: true });
+// };
+
+// export const createProfileRequest = profile => {
+//   const url = "/createProfile";
+//   const data = profile;
+
+//   return axios.post(url, data, { withCredentials: true });
+// };
+
+// export const updatedProfileRequest = (id, profile) => {
+//   const url = `/updatedProfile/${id}`;
+//   const data = profile;
+
+//   return axios.put(url, data, { withCredentials: true });
+// };
+
+// export const getProfileRequest = id => {
+//   const url = `/getProfile/${id}`;
+
+//   return axios.get(url, { withCredentials: true });
+// };
+
 
 
 
